e9a3b458aad72bbaf1b15e1321d3ae94e3b87ef1
saidisaoglu <seidisaoglu@gmail.com>
2024-01-21 12:35:55 +0400

    bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/profile" element={<Profile />} />
       <Route
-        path="/adminPanel/*"
+        path="/adminpanel/*"
         element={
           <ProtectedRoute>
             <AdminPanel />
